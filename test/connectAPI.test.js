ce87b8393ddf72c37471638a114efd5a3e4dff04
M.Charattrakool <m.charattrakool@gmail.com>
2018-06-08 11:18:52 +0700

    add connectAPI test

---
+++ test/connectAPI.test.js
@@ -0,0 +1,61 @@
+import connectAPI from '../src/util/connectAPI'
+import constant from './constant';
+
+it('connectAPI with /applications/create', () => {
+  return connectAPI('/applications/create', constant.public, {
+    product_id: consta.prod_id,
+    form_data: {
+      policy_unit: "Y",
+      insurer_list: [],
+      policy_date: "2018-06-05T06:19:28.938Z",
+      expiry_date: "2019-06-05T06:19:28.938Z",
+      countries: [
+        "WORLDWIDE"
+      ]
+    },
+  }).then(data => expect(data.status).toEqual("ok"))
+})
+
+it('connectAPI with /applications/update', () => {
+  expect.assertions(1)
+  return connectAPI('/applications/update', constant.public, {
+    application_id: constant.app_id,
+    form_data: {
+      policy_unit: 'Y',
+      insurer_list: [{
+        card_type: 'I',
+        first_name: 'TEST',
+        last_name: 'TEST',
+        address: {
+          address_no: '56',
+          postal_code: '11140',
+          province: 'Nonthaburi',
+          district: 'Bangyai',
+          subdistrict: 'Saothonghin'
+        },
+        title: 'MR.',
+        id_card: '1111111111119',
+        birthdate: '1990-01-31T06:20:01.789Z',
+        email: 'test@test.com',
+        phone: '0999999999',
+        nominee: null
+      }],
+      policy_date: '2018-06-05T06:19:28.938Z',
+      expiry_date: '2019-06-05T06:19:28.938Z',
+      countries: [
+        'WORLDWIDE'
+      ],
+      customer_title: 'MR.',
+      customer_first_name: 'TEST',
+      customer_last_name: 'TEST',
+      card_type: 'I',
+      id_card: '1111111111119',
+      email: 'test@test.com',
+      phone: '0999999999',
+      company_name: 'TEST'
+    },
+    insurer_package_code: 'ITA0403',
+    amount: 5196.76,
+    amount_with_tax: 5583,
+  }).then(data => expect(data).toMatchSnapshot())
+})
